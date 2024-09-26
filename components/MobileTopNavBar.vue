823c788c295985a36a5eed2bf0722a38fa21f8af
fido1hn <confidoekeanya@gmail.com>
2024-09-26 05:11:21 +0100

    added mobile navbar

---
+++ components/MobileTopNavBar.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="flex items-center justify-between border-b px-5 py-2">
+    <NuxtLink to="/" class="flex items-center gap-1">
+      <img src="/icon.svg" alt="Website Logo" class="h-10 w-10" />
+      <span class="text-xl font-semibold">Grill Ledger</span>
+    </NuxtLink>
+
+    <div class="flex items-center">
+      <ClientOnly>
+        <ColorModeSelector class="mr-4" />
+      </ClientOnly>
+
+      <div>
+        <UButton
+          variant="ghost"
+          size="xl"
+          icon="i-heroicons-bars-3-center-left-16-solid"
+          @click="isOpen = true" />
+        <USlideover v-model="isOpen">
+          <UCard
+            class="flex flex-1 flex-col"
+            :ui="{
+              body: { base: 'flex-1' },
+              ring: '',
+              divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+            }">
+            <template #header>
+              <UButton
+                color="primary"
+                variant="ghost"
+                size="sm"
+                icon="i-heroicons-x-mark-20-solid"
+                class="absolute end-5 top-5 z-10 flex sm:hidden"
+                square
+                padded
+                @click="isOpen = false" />
+              <div class="flex items-center gap-3">
+                <img
+                  src="/public/icon.svg"
+                  alt="Untitled UI Logo"
+                  class="ml-1.5 h-8 w-8" />
+                <h2 class="text-lg font-semibold">Grill Ledger</h2>
+              </div>
+            </template>
+            <UVerticalNavigation
+              v-if="isLoggedIn"
+              @click="isOpen = false"
+              :links="loggedInLinks"
+              :ui="{
+                padding: 'py-2',
+                font: 'font-meduim',
+                size: 'text-lg',
+                base: 'gap-3',
+                inactive: 'text-gray-700 dark:text-gray-300',
+                icon: {
+                  inactive: 'text-gray-500',
+                  base: 'h-6 w-6',
+                },
+              }"
+              class="items-center" />
+            <UVerticalNavigation
+              v-else
+              @click="isOpen = false"
+              :links="loggedOutLinks"
+              :ui="{
+                padding: 'py-2',
+                font: 'font-meduim',
+                size: 'text-lg',
+                base: 'gap-3',
+                inactive: 'text-gray-700 dark:text-gray-300',
+                icon: {
+                  inactive: 'text-gray-500',
+                  base: 'h-6 w-6',
+                },
+              }"
+              class="items-center" />
+            <template #footer v-if="isLoggedIn">
+              <div class="flex items-center justify-between">
+                <div class="flex gap-4">
+                  <UAvatar size="md" :src="avatarUrl" :alt="userFullName" />
+                  <div class="">
+                    <p class="text-md font-semibold">{{ userFullName }}</p>
+                    <p class="text-sm">{{ userEmailAddress }}</p>
+                  </div>
+                </div>
+                <UButton
+                  icon="i-heroicons-arrow-right-on-rectangle-solid"
+                  size="xl"
+                  variant="ghost"
+                  @click="signOut" />
+              </div>
+            </template>
+          </UCard>
+        </USlideover>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const isOpen = ref(false);
+const supabase = useSupabaseClient();
+const user = useSupabaseUser();
+const { userFullName, userEmailAddress } = useUserDetails();
+const { avatarUrl } = useAvatarUrl();
+
+const isLoggedIn = computed(() => !!user.value);
+
+const loggedInLinks = [
+  [
+    {
+      label: 'Point Of Sale',
+      icon: 'i-heroicons-chart-bar-square',
+      to: '/',
+    },
+    {
+      label: 'Reporting',
+      icon: 'i-heroicons-chart-pie',
+      to: '/reports',
+    },
+  ],
+];
+
+const loggedOutLinks = [
+  [
+    {
+      label: 'Login',
+      icon: 'i-heroicons-arrow-left-on-rectangle-20-solid',
+      to: '/login',
+    },
+    {
+      label: 'Sign Up',
+      icon: 'i-heroicons-user-plus',
+      to: '/login',
+    },
+  ],
+];
+
+async function signOut() {
+  await supabase.auth.signOut();
+  return (window.location.href = '/');
+}
+</script>
