3a242938c4bde2d2bfcfa7a64efa7737b20464a5
fido1hn <confidoekeanya@gmail.com>
2024-10-16 22:08:56 +0100

    update: copy

--- pages/reports.vue
+++ pages/reports.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-6">
-    <h2 class="text-xl font-semibold">Sales History</h2>
+    <h2 class="text-xl font-semibold">Today's Sales History</h2>
     <SalesHistoryTable />
   </div>
 </template>
